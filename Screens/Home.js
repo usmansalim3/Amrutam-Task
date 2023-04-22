1b3e85fdae38e1e874fa9b866d746408033ea736
usmansalim3 <usmansalim3@gmail.com>
2023-04-22 11:20:34 +0530

    Update Home.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -14,53 +14,19 @@ import { AntDesign } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
 import { useSelector } from 'react-redux'
 import DoctorCard from '../Components/DoctorCard'
+import Counter from '../Components/Counter'
+import { blogsData, categories, doctors, infoData, productsCategoriesData, productsData } from '../Data/Data'
+
+
 
 
 const Home = () => {
   const navigation=useNavigation();
   const {appointmentNotifications}=useSelector(state=>state.bulletin)
-  const infoData=[
-    {icon:"moon",message:"You slept for",boldMessage:" 8 hours",iconRight:"reload1"},
-    {icon:"shoe-prints",message:"You took",boldMessage:" 1200 steps",iconRight:"reload1"},
-    {icon:"clock",message:"Screen time is",boldMessage:" 4 hours",iconRight:"reload1"},
-    {icon:"heartbeat",message:"Connect your",boldMessage:" health app",iconRight:"plus",helperMessage:"Health data is not available"}
-  ]
-  const productsCategoriesData=[
-    {path:require("../assets/pills-svgrepo-com.png"),title:"Pills"},
-    {path:require("../assets/beauty-salon-svgrepo-com.png"),title:"Hair"},
-    {path:require("../assets/female-svgrepo-com.png"),title:"Women"},
-    {path:require("../assets/sexual-reproductive-health-svgrepo-com.png"),title:"Sexual"},
-    {path:require("../assets/digestive-system-svgrepo-com.png"),title:"Digestion"}
-  ]
-  const productsData=[
-    {path:require("../assets/Product1.jpeg"),title:"Amrutam Chawanprash",price:"1,200",priceBefore:"1,500"},
-    {path:require("../assets/Product2.png"),title:"Amrutam Gulkand",price:"1,200",priceBefore:"1,800"},
-    {path:require("../assets/Product3.png"),title:"Amrutam Triphala Churna",price:"999"},
-    {path:require("../assets/Product4.png"),title:"Amrutam Zeo Malt",price:"499"},
-    {path:require("../assets/Product5.png"),title:"Amrutam Nari Sondarya Oil",price:"499"}
-  ]
-  const blogsData=[
-    {img:require("../assets/blog4.png"),blog:"1 in 10 women suffer from PCOS"},
-    {img:require("../assets/blog3.png"),blog:"1 in 5 men are prone to baldness by age 20"},
-    {img:require("../assets/blog2.png"),blog:"Acne is the most common skin concern worldwide"},
-    {img:require("../assets/blog1.png"),blog:"1 in 4 adults experience a mental health concern at least once their life"}
-  ]
-  const categories=[
-    {img:require("../assets/quiz.png"),title:"Take a quiz"},
-    {img:require("../assets/phone.png"),title:"E-books"},
-    {img:require("../assets/tv.png"),title:"Amrutam TV"},
-    {img:require("../assets/icons8-product-100.png"),title:"Products"}
-  ]
-
-  const doctors=[
-    {img:"https://cdn.shopify.com/s/files/1/0645/9650/8925/files/312829393_511312870629646_4958697370998722289_n_600x600.jpg?v=1677565430",name:"Dr. Tanuja Gohane"},
-    {img:"https://cdn.shopify.com/s/files/1/0645/9650/8925/files/IMG_20221019_143409_600x600.jpg?v=1675852390",name:"Dr Ravi Dhaliya"},
-    {img:"https://cdn.shopify.com/s/files/1/0645/9650/8925/files/312829393_511312870629646_4958697370998722289_n_250x250.jpg?v=1677565430",name:"Dr. Tanuja Gohane"},
-    {img:"https://cdn.shopify.com/s/files/1/0645/9650/8925/files/312829393_511312870629646_4958697370998722289_n_250x250.jpg?v=1677565430",name:"Dr. Tanuja Gohane"}
-  ]
   function pressHandler(){
     navigation.navigate('blogs')
   }
+  console.log("rerender")
   return (
     // <View style={{flex:1,backgroundColor:"#fff",paddingHorizontal:7,marginBottom:5}}>
       <VirtualizedList>
@@ -71,8 +37,6 @@ const Home = () => {
       <View style={{paddingHorizontal:7,marginVertical:10}}>
         <ReactionSlider/>
       </View>
-
-
         <FlatList
         style={{paddingHorizontal:7}}
         overScrollMode='never'
